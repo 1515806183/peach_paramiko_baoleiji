a69f612d1c3c57c7832bd32342e399652def8efd
1515806183 <1515806183@qq.com>
2018-12-21 19:20:09 +0800

    Initial commit

---
+++ peach/conf/settings.py
@@ -0,0 +1,24 @@
+#_*_coding:utf-8_*_
+__author__ = 'Peach'
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+DB_CONN = "mysql+pymysql://root:t505514601@cdb-dg1tu5bj.gz.tencentcdb.com:10016/baolei_ji?charset=utf8"
+
+'''
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysqldb',
+        'NAME': 'LittleFinger',
+        'HOST': '',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD': ''
+    }
+}
+'''
+
